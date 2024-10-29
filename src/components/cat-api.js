32c9ba7c3de211a3e30fb00a74f57bc5b4ed6c22
Jigsaw28 <levenko7@gmail.com>
2024-10-29 21:08:43 +0200

    subject

--- src/components/cat-api.js
+++ src/components/cat-api.js
@@ -14,7 +14,7 @@ export function fetchBreeds() {
 
 export function fetchCatByBreed(breedId) {
   return axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .get(`https://api.thecatapi.com/v1/images/${breedId}`)
     .then(response => response.data)
     .catch(error => {
       console.log(error);
--- src/index.js
+++ src/index.js
@@ -1,31 +1,72 @@
-import { fetchBreeds } from './cat-api';
-import { fetchCatByBreed } from './cat-api';
+import { fetchBreeds } from './components/cat-api';
+import { fetchCatByBreed } from './components/cat-api';
+import { Notify } from 'notiflix';
+import SlimSelect from 'slim-select';
 
-const selectEl = document.querySelector('.breed-select');
-const catInfoEl = document.querySelector('.cat-info');
+const refs = {
+  selectEl: document.querySelector('.breed-select'),
+  catInfoEl: document.querySelector('.cat-info'),
+  loaderEl: document.querySelector('.loader'),
+  // errorEl: document.querySelector('.error'),
+};
+
+const { selectEl, catInfoEl, loaderEl } = refs;
+
+// errorEl.classList.add('is-hidden');
+selectEl.classList.add('is-hidden');
+catInfoEl.classList.add('is-hidden');
+
+
 
 fetchBreeds()
-  .then(arrayBreeds =>
-    selectEl.insertAdjacentHTML('beforeend', renderBreeds(arrayBreeds))
-  )
-  .catch(error => console.log(error));
+  .then(arrayBreeds => {
+    selectEl.insertAdjacentHTML('beforeend', renderBreeds(arrayBreeds));
+    showInfo(loaderEl, selectEl);
+  })
+  .catch(error => {
+    loaderEl.classList.add('is-hidden');
+    Notify.failure('Oops! Something went wrong! Try reloading the page!')
+    console.log(error);
+  });
 
 selectEl.addEventListener('change', onChange);
 
 function onChange(e) {
-  fetchCatByBreed(e.target.value).then(breeds =>
-    catInfoEl.insertAdjacentHTML('beforeend', renderBreedsImage(breeds))
-  );
+  catInfoEl.innerHTML = '';
+  loaderEl.classList.remove('is-hidden');
+
+  fetchCatByBreed(e.target.value)
+    .then(data => {
+      catInfoEl.insertAdjacentHTML('beforeend', renderBreedsInfo(data));
+      showInfo(loaderEl, catInfoEl);
+    })
+    .catch(error => {
+      loaderEl.classList.add('is-hidden');
+      Notify.failure('Oops! Something went wrong! Try reloading the page!')
+      console.log(error);
+    });
 }
 
 function renderBreeds(breeds) {
   return breeds
-    .map(breed => `<option value=${breed.id}>${breed.name}</option>`)
+    .map(
+      breed =>
+        `<option value=${breed.reference_image_id}>${breed.name}</option>`
+    )
     .join('');
 }
 
-function renderBreedsImage(breeds) {
-  return breeds.map(breed => {
-    return `<img src=${breed.url} width=450px height=300px/>`;
-  });
+function renderBreedsInfo(data) {
+  return data.breeds.map(
+    breed => `<img src=${data.url} />
+    <h1>${data.breeds[0].name}</h1>
+    <p>${data.breeds[0].description}</p>
+    <h3>${data.breeds[0].temperament}</h3>`
+  );
 }
+
+function showInfo(loader, elInfo) {
+  elInfo.classList.remove('is-hidden');
+  loader.classList.add('is-hidden');
+}
+
