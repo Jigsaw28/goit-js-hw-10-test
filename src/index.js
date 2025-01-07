52bff658df55c8fcad4035dc03c380bd9b43c5be
Jigsaw28 <levenko7@gmail.com>
2025-01-07 15:26:08 +0200

    done task

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import { fetchBreeds } from './components/cat-api';
 import { fetchCatByBreed } from './components/cat-api';
 import { Notify } from 'notiflix';
-import SlimSelect from 'slim-select';
 
 const refs = {
   selectEl: document.querySelector('.breed-select'),
   catInfoEl: document.querySelector('.cat-info'),
   loaderEl: document.querySelector('.loader'),
-  // errorEl: document.querySelector('.error'),
 };
 
 const { selectEl, catInfoEl, loaderEl } = refs;
 
-// errorEl.classList.add('is-hidden');
 selectEl.classList.add('is-hidden');
 catInfoEl.classList.add('is-hidden');
-
-
 
 fetchBreeds()
   .then(arrayBreeds => {
@@ -25,7 +20,7 @@ fetchBreeds()
   })
   .catch(error => {
     loaderEl.classList.add('is-hidden');
-    Notify.failure('Oops! Something went wrong! Try reloading the page!')
+    Notify.failure('Oops! Something went wrong! Try reloading the page!');
     console.log(error);
   });
 
@@ -34,7 +29,7 @@ selectEl.addEventListener('change', onChange);
 function onChange(e) {
   catInfoEl.innerHTML = '';
   loaderEl.classList.remove('is-hidden');
-
+  
   fetchCatByBreed(e.target.value)
     .then(data => {
       catInfoEl.insertAdjacentHTML('beforeend', renderBreedsInfo(data));
@@ -42,26 +37,25 @@ function onChange(e) {
     })
     .catch(error => {
       loaderEl.classList.add('is-hidden');
-      Notify.failure('Oops! Something went wrong! Try reloading the page!')
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
       console.log(error);
     });
 }
 
 function renderBreeds(breeds) {
-  return breeds
-    .map(
-      breed =>
-        `<option value=${breed.reference_image_id}>${breed.name}</option>`
-    )
-    .join('');
+ return  breeds.map(
+    breed =>
+      `<option value=${breed.reference_image_id}>${breed.name}</option>`
+  )
+  .join('');
 }
 
 function renderBreedsInfo(data) {
   return data.breeds.map(
-    breed => `<img src=${data.url} />
-    <h1>${data.breeds[0].name}</h1>
+    breed => `<img src=${data.url} class='cat-img' />
+    <div class='cat-container'><h1>${data.breeds[0].name}</h1>
     <p>${data.breeds[0].description}</p>
-    <h3>${data.breeds[0].temperament}</h3>`
+    <span id="temp">Temperament:&nbsp;<h3>${data.breeds[0].temperament}</h3></div>`
   );
 }
 
@@ -69,4 +63,3 @@ function showInfo(loader, elInfo) {
   elInfo.classList.remove('is-hidden');
   loader.classList.add('is-hidden');
 }
-
